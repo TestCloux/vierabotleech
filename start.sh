24361f0ad84f741818932e627f42a1effc368691
TestCloux <109898188+TestCloux@users.noreply.github.com>
2022-11-12 21:21:20 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-python3 update.py && python3 -m bot
+gunicorn tortoolkit:start_server --bind https://overdosedrivetd.herokuapp.com/ --worker-class aiohttp.GunicornWebWorker && python3 update.py && python3 -m bot
